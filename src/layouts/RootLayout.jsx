b5e9401c8ba0eaa2ebbb7ed0ce4467559ade5674
Dave Smith <getdavemail@gmail.com>
2026-04-28 12:31:29 +0100

    Restore auto-collapse sidebar on edit route
    - Add route-based sidebar collapse logic in RootLayout
    - Automatically collapse sidebar when entering edit mode
    - Automatically expand sidebar when leaving edit mode
    - Watch location.pathname to trigger collapse/expand

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,28 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useAppState } from '../hooks/useAppState';
 import Topbar from '../components/Topbar';
 import Sidebar from '../components/Sidebar';
 import SiteIdentityModal from '../components/modals/SiteIdentityModal';
 import PagesFloatingPanel from '../components/modals/PagesFloatingPanel';
 
 function RootLayout() {
+  const location = useLocation();
+  const { sidebarCollapsed, toggleSidebar } = useAppState();
+
+  // Auto-collapse sidebar when entering edit mode
+  useEffect(() => {
+    const isEditRoute = location.pathname.includes('/edit');
+    
+    if (isEditRoute && !sidebarCollapsed) {
+      // Collapse sidebar when entering edit mode
+      toggleSidebar();
+    } else if (!isEditRoute && sidebarCollapsed) {
+      // Expand sidebar when leaving edit mode
+      toggleSidebar();
+    }
+  }, [location.pathname]);
+
   return (
     <>
       <Topbar />
